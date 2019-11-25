5763c086553eb3c623a8124250f3a9944f172177
Sidon Duarte <sidoncd@gmail.com>
2019-11-24 22:07:46 -0300

    Update setup for dependencies

--- setup.py
+++ setup.py
@@ -18,5 +18,10 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        'xmltodict==0.12.0'
+    ],
     python_requires='>=3.6',
+
+
 )
