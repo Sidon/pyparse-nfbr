65d36741d6b5cac2df380ea295265d6ec1c59adf
Sidon Duarte <sidoncd@gmail.com>
2019-11-24 23:47:05 -0300

    update setup

--- setup.py
+++ setup.py
@@ -4,24 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyparse-nfbr", 
-    version="0.0.1",
+    name="pyparse-nfbr",
+    version="0.0.1-alfa",
     author="Sidon",
     author_email="sidoncd@gmail.com",
     description="Parse em NFebr",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
     packages=setuptools.find_packages(),
+    url="https://github.com/Sidon/pyparse-nfbr",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-        'xmltodict==0.12.0'
-    ],
-    python_requires='>=3.6',
-
-
+    python_requires='==3.7',
 )
